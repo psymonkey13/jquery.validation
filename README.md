6b95ba139a432b0cb43a4bf7cdf5ddb2e2afa00f
psymonkey13 <psymonkey13@gmail.com>
2018-03-16 16:46:33 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,147 @@
+jQeury v3.2.1 + Bootstrap v4.0 Validation
+===================
+
+
+jQuery 기반으로 유효성체크하는 라이브러리입니다. Bootstrap의 Validation class를 옵션으로 사용할 수 있습니다. 
+
+> **Note:**
+> - jQuery 3.2.1, Bootstrap v4.0 기준으로 제작되었습니다.
+
+
+# Quick start
+#### jQuery [필수]
+```
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+```
+
+#### jQuery library [필수]
+```
+<script src="jquery-library.js"></script>
+```
+
+#### Bootstrap CSS [옵션]
+```
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+```
+
+#### Bootstrap Javascript [옵션]
+```
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+```
+
+#### Sample Source
+```
+<script>
+    function formChecker() {
+        var literal = { 
+            name: { selector: $('#name'), required: { message: '성명을 입력해주세요.' } },
+            pwd: { selector: $('#pwd'), min: { value: 4, message: '비밀번호는 4자리 이상 입력해주세요.' } }
+        };
+        return $.validate.rules(literal, { mode: 'bootstrap' });
+    }
+</script>
+```
+
+# Literal 유형
+```
+var literal = {
+    name: { // literal 항목 이름
+        selector: $('#name"), // jquery selector
+        required: { // validation 항목
+            message : '이름을 입력해주세요.' // 경고 메시지
+        }
+    },
+    check: function() {
+        // 리터럴 함수는 반드시 수행결과에 따른  true 또는 false로 반환해야 된다.
+        // 리터럴 함수의 반환값은 최종결과($.validate.rules)에 영향을 준다.
+        return true;
+    }
+};
+
+return $.validate.rules(literal, { // validation 함수
+    mode: 'bootstrap' // 경고 메시지 유형 alert or bootstrap
+});
+```
+# Options 유형
+    { 
+        mode: '' // alert or bootstrap
+        bootstrap: { // mode 항목이 bootstrap 일때만 적용
+            feedback: 'invalid-feedback',
+            error: 'is-invalid',
+            success: 'is-valid'
+        }
+    }
+
+# Validation 항목
+### 1. required
+    sample: { selector: $('#required'), required: { message: '필드를 입력해주세요.' } }
+    입력값이 있는지 필수 항목을 체크합니다.
+* message: '입력값을 입력해주세요.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 2. digit
+    sample: { selector: $('#digit'), digit: { message: '숫자만 입력해주세요.' } }
+    입력값이 숫자인지 체크합니다.
+* message: '숫자만 입력해주세요.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 3. email
+    sample: { selector: $('#email'), email: { message: '이메일형식을 확인해주세요.' } }
+    입력값이 이메일 형식인지 체크합니다.
+* message: '이메일형식을 확인해주세요.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 4. date
+    sample: { selector: $('#date'), date: { message: '날짜형식을 확인해주세요.\nEx) 2018-03-08' } }
+    입력값이 날짜형식인지 체크합니다. 2018-03-08 형식
+* message: '날짜형식을 확인해주세요.\nEx) 2018-03-08' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 5. min (길이)
+    sample: { selector: $('#min'), min: { value: 2, message: '최소 2자 이상으로 입력하세요.' } }
+    입력값의 최소 길이를 체크합니다.
+* value: 2 - *필수*
+* message: '최소 2자 이상으로 입력하세요.' - *필수*
+* feedback: '#selector' - *옵션*
+### 6. max (길이)
+    sample: { selector: $('#max'), max: { value: 4, message: '최대 4자 이하로 입력하세요.' } }
+    입력값의 최대 길이를 체크합니다.
+* value: 4 - *필수*
+* message: '최대 4자 이하로 입력하세요.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 7. from (숫자)
+    sample: { selector: $('#from'), from: { value: 2, message: '숫자 2이상으로 입력하세요.' } }
+    입력값의 최소 값을 체크합니다.
+* value: 2 - *필수*
+* message: '숫자 2이상으로 입력하세요.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 8. to (숫자)
+    sample: { selector: $('#to'), to: { value: 4, message: '숫자 4이하로 입력하세요.' } }
+    입력값의 최대 값을 체크합니다.
+* value: 4 - *필수*
+* message: '숫자 4이하로 입력하세요.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 9. range (숫자)
+    sample: { selector: $('#range'), range: { value: [2, 4], message: '숫자 2이상 4이하로 입력하세요.' } }
+    입력값의 범위를 체크합니다.
+* value: [2, 4] - *필수*
+* message: '숫자 2이상 4이하로 입력하세요.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 10. compare
+    sample: { selector: $('#compare'), compare: { value: 'test', message: '입력한 값이 test가 아닙니다.' } }
+    입력값과 비교값 일치 여부를 체크합니다.
+* value: 'test' - *필수*
+* message: '입력한 값이 test가 아닙니다.' - *필수*
+* feedback: '#selector' - *옵션*
+
+### 11. regex
+    sample: { selector: $('#regex'), regex: { value: /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, message: '한글로 입력해주세요.' } }
+    입력값이 정규식과 일치 여부를 체크합니다.
+* value: /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/ - *필수*
+* message: '한글로 입력해주세요.' - *필수*
+* feedback: '#selector' - *옵션*
---
+++ jquery-library.js
@@ -0,0 +1,328 @@
+//  Created by psymonkey13 on 2018.03.14
+//  Copyright © 2018 psymonkey13. All rights reserved.
+//  jquery-library v1.0.0
+
+; (function($) {
+    'use strict';
+
+    var defaults = {  // global default options
+        mode: 'bootstrap', // alert or bootstrap
+        bootstrap: {
+            feedback: 'invalid-feedback',
+            error: 'is-invalid',
+            success: 'is-valid'
+        }
+    };
+    var options = { }; // global options
+    
+    $.validate = {
+        rules: function(literal, opts) {
+            try {
+                var property, 
+                    check = true, 
+                    array = [];
+                
+                options = $.extend({}, defaults, opts);
+                
+                // 리터럴 전체 each
+                $.each(literal, function(key, item) {
+                    if ($.type(item) === 'function') {
+                        array.push({ result: item(), type: 'function' });
+                    } else if ($.type(item) === 'object') {
+                        property = $.validate.property(item.selector);
+                        delete item.selector;
+                        $.each($.validate.validator(property, item), function(index, item) {
+                            array.push(item);
+                            if (!item.result && options.mode === 'alert') {
+                                check = false;
+                                return false; // exit each
+                            }
+                        });
+                    } else {
+                        throw 'literal ' + key + ' must be function or object.';
+                    }
+                    
+                    if (!check) {
+                        return false; // exit each
+                    }
+                });
+                
+                return $.validate.feedback(array);
+            } catch (e) {
+                if (window.console && window.console.log) {
+                    window.console.log(e);
+                }
+                return false;
+            }
+        },
+        validator: function(property, literal) {
+            var array = [],
+            result = false;
+            
+            $.each(literal, function(key, item) {
+                result = validatorResult(key, property.value, item.value);
+                array.push({ result: result, type: 'object', checker: key, property: property, literal: item });
+                if (!result && options.mode === 'alert') {
+                    return false;
+                }
+            });
+            
+            return array;
+        },
+        property: function(selector) {
+            var attribute = { },
+            value;
+            
+            attribute.id = selector.attr('id');
+            attribute.selector = selector;
+            attribute.type = selector.attr('type');
+            attribute.tag = selector[0].tagName.toLowerCase();
+            
+            // select type 속성 할당
+            if (attribute.tag === 'select') {
+                attribute.type = 'select';
+            }
+            
+            // 값 할당
+            if (attribute.type === 'radio' || attribute.type === 'checkbox') {
+                var array = [];
+                $(selector).each(function() {
+                    if ($(this).is(':checked')) {
+                        array.push($(this).val());
+                    }
+                    value = array.join(',');
+                });
+                if (value === undefined) {
+                    value = '';
+                }
+            } else {
+                value = selector.val();
+            }
+            attribute.value = value;
+            
+            // event 할당
+            if (attribute.type === 'checkbox' || attribute.type === 'radio') {
+                attribute.event = 'validate.click'
+            } else if (attribute.type === 'select') {
+                attribute.event = 'validate.change'
+            } else {
+                attribute.event = 'validate.keyup'
+            }
+            
+            return attribute;
+        },
+        feedback: function(array) {
+            var check = true;
+            if (options.mode === 'bootstrap') {
+                $.each(array, function(index, item) {
+                    if (item.type === 'object') {
+                        item.property.selector.parent().find('.'+ options.bootstrap.feedback).remove();
+                    }
+                });
+            }
+            
+            $.each(array, function(index, item) {
+                if (!item.result && item.type === 'object') {
+                    check = false;
+                    var $selector = item.property.selector;
+                    if (options.mode === 'alert') {
+                        alert(item.literal.message);
+                        $selector.focus();
+                        return false;
+                    } else if (options.mode === 'bootstrap') {
+                        if (item.literal.feedback !== undefined) {
+                            $(item.literal.feedback).text(item.literal.message);
+                        } else { 
+                            if (item.property.type !== 'radio' && item.property.type !== 'checkbox') {
+                                $selector.parent().append($('<div />', { addClass: options.bootstrap.feedback, text: item.literal.message }));
+                            }
+                        }
+                        $selector.addClass(options.bootstrap.error);
+                        $selector.trigger(item.property.event, [item.checker, item.literal, item.property]);
+                    }
+                }
+            });
+            
+            return check;
+        },
+        required: function(value) {
+            if (!$.library.none(value)) {
+                return true;
+            }
+            return false;
+        },
+        digit: function(value) {
+            if ($.isNumeric(value)) {
+                return true;
+            }
+            return false;
+        },
+        min: function(value, length) {
+            if (value.length >= length) {
+                return true;
+            }
+            return false;
+        },
+        max: function(value, length) {
+            if ($.isNumeric(value)) {
+                if (value.length <= length) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        from: function(value, length) {
+            if ($.isNumeric(value)) {
+                if (value >= length) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        to: function(value, length) {
+            if ($.isNumeric(value)) {
+                if (value <= length) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        range: function(value, array) {
+            if ($.isNumeric(value)) {
+                if (array.length !== 2) {
+                    return false;
+                } else {
+                    var from = array[0],
+                    to = array[1];
+                    
+                    if (value >= from && value <= to) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        },
+        email: function(value) {
+            var rule = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            
+            if (rule.test(value)) {
+                return true;
+            }
+            return false;
+        },
+        compare: function(value, target) {
+            if (!$.library.none(value) && !$.library.none(target)) {
+                if (value === target) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        date: function(value) {
+            if ($.library.date(value)) {
+                return true;
+            }
+            return false;
+        },
+        regex: function(value, express) {
+            if (express.test(value)) {
+                return true;
+            }
+            return false;
+        }
+    };
+    
+    $.library = {
+        none: function(value) {
+            if ($.trim(value) === '' || value === undefined || value === null) {
+                return true;
+            }
+            return false;
+        },
+        date: function(value) {
+            var bits = value.split('-'),
+            yyyy = bits[0],
+            mm = bits[1],
+            dd = bits[2],
+            months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            
+            if ((!(yyyy % 4) && yyyy % 100) || !(yyyy % 400)) {
+                months[1] = 29;
+            }
+            return !(/\D/.test(String(dd))) && dd > 0 && dd <= months[--mm]
+        },
+        array: function(array, value) {
+            var length = array.length;
+            for (var i = 0; i < length; i++) {
+                if (value.indexOf(array[i]) !== -1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+    
+    // 유효성 검사항목을 찾고 결과 반환 -custom 이벤트에서만 사용!
+    function validatorResult(checker, value1, value2) {
+        var result = false
+        if ($.library.array(['required', 'digit', 'email', 'date'], checker)) {
+            result = $.validate[checker](value1);
+        } else {
+            if (litervalValue !== undefined) {
+                result = $.validate[checker](value1, value2);
+            }
+        }
+        
+        return result;
+    }
+    
+    // Bootstrap 유효성검사 Toggle
+    function validatorToggle(result, selector, literal) {
+        if (result) {
+            selector.removeClass(options.bootstrap.error);
+            selector.addClass(options.bootstrap.success);
+        } else {
+            selector.removeClass(options.bootstrap.success);
+            selector.addClass(options.bootstrap.error);
+        }
+        
+        if (literal.feedback !== undefined) {
+            if (!result) {
+                $(literal.feedback).text(literal.message);
+            } else {
+                $(literal.feedback).text('');
+            }
+        }
+    }
+    
+    // validate event 키보드 이벤트
+    $(document).on('validate.keyup', function(e, checker, literal, property) {
+        property.selector.off().on('keyup', function(event) {
+            var result = validatorResult(checker, $(this).val(), literal.value);
+            validatorToggle(result, property.selector, literal);
+        });
+    });
+    
+    // validate event 클릭 이벤트
+    $(document).on('validate.click', function(e, checker, literal, property) {
+        property.selector.off().on('click', function() {
+            var result = false;
+            property.selector.each(function() {
+                if ($(this).is(':checked')) {
+                    result = true;
+                    return false; // each
+                }
+            });
+            
+            validatorToggle(result, property.selector, literal);
+        });
+    });
+    
+    // validate event 변경 이벤트
+    $(document).on('validate.change', function(e, checker, literal, property) { 
+        property.selector.off().on('change', function() {
+            var result = !$.library.none($(this).val()) ? true : false;
+            validatorToggle(result, property.selector, literal);
+        });
+    });
+}(jQuery));
